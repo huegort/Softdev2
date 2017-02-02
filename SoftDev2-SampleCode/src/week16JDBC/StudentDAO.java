0ddaecd9357b3c5bdf61e35345ec5af99c9739bc
ITS <ITS@10.0.2.15>
2017-02-02 15:27:27 +0000

    a fuller DAO

--- SoftDev2-SampleCode/src/week16JDBC/StudentDAO.java
+++ SoftDev2-SampleCode/src/week16JDBC/StudentDAO.java
@@ -3,8 +3,10 @@ package week16JDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDAO {
@@ -15,19 +17,24 @@ public class StudentDAO {
 
 	static String dbURL="jdbc:mysql://"+dbHost+"/"+dbDatabase+"?user="+dbUser+"&password="+dbPassword;
 
-
-	public void create(Student student){
+	private Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException("driver not loaded");
 		}
 
-		Connection connection = null;
+		try {
+			return  DriverManager.getConnection(dbURL);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+	public void create(Student student){
+		
+		Connection connection = getConnection();
 		try{
-			connection = DriverManager.getConnection(dbURL);
-
-
 			String sql = "insert into student (id, name, age) values (?,?,?);";
 			PreparedStatement statement = connection.prepareStatement(sql);
 			statement.setLong(1, student.getId());
@@ -50,15 +57,154 @@ public class StudentDAO {
 
 	}
 	public void update(Student student){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("driver not loaded",e);
+		}
 
+		Connection connection = null;
+		try{
+			connection = DriverManager.getConnection(dbURL);
+			String sql = "update student set name= ?, age=? where id = ?;";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setLong(3, student.getId());
+			statement.setString(1, student.getName());
+			statement.setInt(2, student.getAge());
+			
+			statement.executeUpdate();
+		}catch (SQLException sqle){
+			throw new RuntimeException(sqle);
+		}finally{
+			if (connection!=null){
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
 	}
+	public void incrementAges(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("driver not loaded",e);
+		}
+
+		Connection connection = null;
+		try{
+			connection = DriverManager.getConnection(dbURL);
+			String sql = "update student set age= age+1;";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			
+			statement.executeUpdate();
+		}catch (SQLException sqle){
+			throw new RuntimeException(sqle);
+		}finally{
+			if (connection!=null){
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+	}
+	
 	public void delete(Student student){
 
 	}
 	public Student findById(long id){
-		return null;
+		Student student = null;
+		
+		Connection connection = getConnection();
+		try{
+			String sql = "select * from student where id = ?;";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setLong(1, id);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()){
+				student = new Student();
+				student.setId(rs.getLong("id"));
+				student.setName(rs.getString("name"));
+				student.setAge(rs.getInt("age"));
+			}
+			
+		}catch(SQLException sqle){
+			throw new RuntimeException(sqle);
+		}finally{
+			if (connection != null){
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		return student;
 	}
 	public List<Student> getAll(){
-		return null;
+		List<Student> students = new ArrayList<Student>();
+		
+		Connection connection = getConnection();
+		try{
+			String sql = "select * from student;";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			
+			while (rs.next()){
+				Student student = new Student();
+				student.setId(rs.getLong("id"));
+				student.setName(rs.getString("name"));
+				student.setAge(rs.getInt("age"));
+				students.add(student);
+			}
+			
+		}catch(SQLException sqle){
+			throw new RuntimeException(sqle);
+		}finally{
+			if (connection!=null){
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		
+		return students;
+	}
+	public List<Student> getAllOlderThan(int age){
+		List<Student> students = new ArrayList<Student>();
+		
+		Connection connection = getConnection();
+		try{
+			String sql = "select * from student where age > ?;";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setInt(1, age);
+			ResultSet rs = statement.executeQuery();
+			
+			while (rs.next()){
+				Student student = new Student();
+				student.setId(rs.getLong("id"));
+				student.setName(rs.getString("name"));
+				student.setAge(rs.getInt("age"));
+				students.add(student);
+			}
+			
+		}catch(SQLException sqle){
+			throw new RuntimeException(sqle);
+		}finally{
+			if (connection!=null){
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		
+		return students;
 	}
 }
--- SoftDev2-SampleCode/src/week16JDBC/TestStudentDAO.java
+++ SoftDev2-SampleCode/src/week16JDBC/TestStudentDAO.java
@@ -1,11 +1,28 @@
 package week16JDBC;
 
+import java.util.List;
+
 public class TestStudentDAO {
 
 	public static void main(String[] args) {
-		Student student = new Student(12,"mick",21);
+		Student student = new Student(14,"joe",22);
 		StudentDAO dao = new StudentDAO();
-		dao.create(student);
+		//dao.create(student);
+		//dao.incrementAges();
+		
+		//student = dao.findById(14);
+		//System.out.println(student);
+		
+		List<Student> students = dao.getAll();
+		for (Student oneStudent : students){
+			System.out.println(oneStudent);
+		}
+		System.out.println("older students");
+		students = dao.getAllOlderThan(24);
+		for (Student oneStudent : students){
+			System.out.println(oneStudent);
+		}
+		
 
 	}
 
